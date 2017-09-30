2d7ea72e7cef861435b1272f9d9fe9b050142bc9
Slobodan Vrcelj <slobodan.vrcelj79@gmail.com>
2017-09-30 20:16:29 +0200

    no message

---
+++ main.py
@@ -0,0 +1,57 @@
+import os
+import logging
+import jinja2
+import webapp2
+
+
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=False)
+
+
+class BaseHandler(webapp2.RequestHandler):
+
+    def write(self, *a, **kw):
+        return self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        return self.write(self.render_str(template, **kw))
+
+    def render_template(self, view_filename, params=None):
+        if params is None:
+            params = {}
+        template = jinja_env.get_template(view_filename)
+        return self.response.out.write(template.render(params))
+
+
+class MainHandler(BaseHandler):
+    def get(self):
+        return self.render_template("hello.html")
+
+    def post(self):
+        first_number = float(self.request.get("first number"))
+        second_number = float(self.request.get("second number"))
+        operation = self.request.get("operation")
+
+        logging.info("operacija: " + operation)
+
+        result = None
+
+        if operation == "+":
+            result = first_number + second_number
+        elif operation == "-":
+            result = first_number - second_number
+        elif operation == "*":
+            result = first_number * second_number
+        elif operation == "/":
+            result = first_number / second_number
+
+        return self.write("Your result is %s" % result )
+
+
+app = webapp2.WSGIApplication([
+    webapp2.Route('/', MainHandler),
+], debug=True)
